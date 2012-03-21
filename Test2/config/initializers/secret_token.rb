747553642d6b406febfe0fc2e941d3959f8ab9c4
Chris Knuteson <cknuteson@gannett.com>
2012-03-21 15:54:13 -0400

    first commit

---
+++ Test2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test2::Application.config.secret_token = '224200f9fcd977589256f7aab6c00fac7c40c47337dbb7bbba0efe5d2973b4e82ddc46535860ba19ae2e0ce15c18cfc6cf229b5525fbb5d38c3cbb7fd5dae150'
